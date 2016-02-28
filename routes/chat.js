708c93d17acd24ed624ca884a54547ffe4100395
Tarabass <p.rietveld@live.com>
2016-02-28 22:09:20 +0100

    Cleanup some code after introducing chat rooms

--- routes/chat.js
+++ routes/chat.js
@@ -66,8 +66,8 @@ var express = require('express');
 var router = express.Router();
 var io = require('socket.io');
 
-var chat123;
-var room123;
+var currentNameSpace;
+var currentRoom;
 
 // parameter middleware that will run before the next routes
 router.param('room', function(req, res, next, room) {
@@ -75,55 +75,47 @@ router.param('room', function(req, res, next, room) {
 
 	// add -private to the room
 	// TODO: add guid (instead of private)to the room to make it unique
-	var modified = room + '-private';
-
-	// save name to the request
-	req.room = modified;
+	currentRoom = room + '-private';
 
 	var nameSpace = req.baseUrl;
 	var server = req.client.server;
 
-	if(!chat123) {
+	if(!currentNameSpace) {
 		/**
 		 * Initialize SocketIO and create namespace
 		 */
-		chat123 = io(server).of(nameSpace); // TODO: server is undefined, we have to pass it through from www
-		chat123.on('connection', onSocketIOConnection);
+		currentNameSpace = io(server).of(nameSpace);
+		currentNameSpace.on('connection', onSocketIOConnection);
 	}
-	room123 = modified;
 
 	next();
 });
 
 /* GET /chat */
 router.get('/:room', function(req, res, next) {
-	// http://localhost:1234/chat/myroom
-	console.log('params.room', req.params.room); // 'myroom'
-	console.log('room', req.room); // 'myroom-private'
-	console.log('url', req.url); // '/myroom'
+	//// http://localhost:1234/chat/myroom
+	//console.log('params.room', req.params.room); // 'myroom'
+	//console.log('url', req.url); // '/myroom'
+	//
+	//console.log('originalUrl', req.originalUrl); // '/chat/myroom'
+	//console.log('baseUrl', req.baseUrl); // '/chat'
+	//console.log('path', req.path); // '/myroom'
+	//
+	//var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+	//console.log(fullUrl); // 'http://localhost:1234/chat/myroom'
 
-	console.log('originalUrl', req.originalUrl); // '/chat/myroom'
-	console.log('baseUrl', req.baseUrl); // '/chat'
-	console.log('path', req.path); // '/myroom'
-
-	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-	console.log(fullUrl); // 'http://localhost:1234/chat/myroom'
-
-	res.render('chat', { title: 'Chat' });
+	res.render('chat', { title: currentRoom });
 });
 
 /**
  * Event listener for socket.io "connection" event.
  */
 function onSocketIOConnection(socket) {
-	var room = room123;//'chatroom 1';
-
-	socket.join(room);
+	// Join current room
+	socket.join(currentRoom);
 
 	socket.on('chat message', function(message) {
-		console.log('message: ' + message);
-		console.log('room: ' + room);
-		chat123.to(room).emit('chat message', room + ': ' + message);
+		currentNameSpace.to(currentRoom).emit('chat message', currentRoom + ': ' + message);
 	});
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
